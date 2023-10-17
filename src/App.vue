82ae91a9fddfe6cbb2862f5a6373d9505b728e08
Vasilina <vasilinamel04@gmail.com>
2023-10-17 20:20:45 +1000

    Основа для разработки

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,28 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <body>
+    <img src="/src/assets/B13A0313.JPG"  class = "My_photo" width="350">
+    <h1 class = "info">Мелихова Василина Андреевна</h1>
+    <p class = "info">Будущий программист - организатор</p>
+    <a href="https://t.me/Vasilina_Melikhova">
+      <img src="/src/assets/telegram_icon_131945.svg" alt="Telegram" class = "icon"></a>
+    <a href="https://vk.com/melikhova2004">
+      <img src="/src/assets/vk-social-network-logo_icon-icons.com_73339.svg" alt="Vk" class = "icon"></a>
+    <a href="https://github.com/vasilina04">
+      <img src="/src/assets/github-logo_icon-icons.com_73546.svg" alt="Github" class = "icon"></a>
+    <h2 class="info">Интересные факты обо мне</h2>
+    <ul>
+     <li>Работаю в творческом коллективе<a href="https://vk.com/tm_kadry">ТМ "Кадры"</a></li> 
+     <li>Учусь на втором курсе Прикладной математики и информатика в ДВФУ</li>
+     <li>Главный принцип "Не ошибается тот, кто ничего не делает."</li>
+     <li>Учусь преподавать</li>
+     <li>Люблю сложные задачи с нестандартыми решением проблемы </li>
+    <li>Работала в приемной комиссии 2023</li>
+   </ul>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </body>
 </template>
 
 <style scoped>
@@ -44,4 +52,19 @@ header {
     flex-wrap: wrap;
   }
 }
+  .My_photo {
+    border-radius: 10px;  /* Скругление углов изображения */
+    border: 5px solid #fff; /* Цвет и ширину рамки */
+    box-shadow: 2px 1px 5px #999999; /* Цвет и размер тени */
+}
+  .icon {
+    width: 48px;
+    height: 48px;
+    margin-left: 12px;
+  }
+  .info{
+    margin-left: 7%;
+    display: flex;
+    place-items: center;
+  }
 </style>
