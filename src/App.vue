e9d156f64e67b1ce8e4c846652cbefabf4cd7171
Vasilina <vasilinamel04@gmail.com>
2023-10-17 23:59:49 +1000

    Добавление анимации

--- src/App.vue
+++ src/App.vue
@@ -2,84 +2,89 @@
 </script>
 
 <template>
-  <body>
-    <img src="/src/assets/B13A0313.JPG"  class = "My_photo" width="350">
-    <h1 class = "info">Мелихова Василина Андреевна</h1>
-    <h4 class = "info">Будущий программист - организатор</h4>
-    <a href="https://t.me/Vasilina_Melikhova">
-      <img src="/src/assets/telegram_icon_131945.svg" alt="Telegram" class = "icon"></a>
-    <a href="https://vk.com/melikhova2004">
-      <img src="/src/assets/vk-social-network-logo_icon-icons.com_73339.svg" alt="Vk" class = "icon"></a>
-    <a href="https://github.com/vasilina04">
-      <img src="/src/assets/github-logo_icon-icons.com_73546.svg" alt="Github" class = "icon"></a>
-    <h2 class="info">Интересные факты обо мне</h2>
-    <ul>
-     <li>Работаю в творческом коллективе<a href="https://vk.com/tm_kadry">ТМ "Кадры"</a></li> 
-     <li>Учусь на втором курсе Прикладной математики и информатика в ДВФУ</li>
-     <li>Главный принцип "Не ошибается тот, кто ничего не делает."</li>
-     <li>Учусь преподавать</li>
-     <li>Люблю сложные задачи с нестандартыми решением проблемы </li>
-    <li>Работала в приемной комиссии 2023</li>
-   </ul>
-
-  </body>
+    <body>
+      <div class="start_info">
+        <img src="/src/assets/B13A0313.JPG" class="My_photo" width="350">
+        <div class="profile">
+          <h1>Мелихова Василина Андреевна</h1>
+          <h3>Будущий программист - организатор</h3>
+          <ul class="nobullet">
+            <li>
+              <a href="https://t.me/Vasilina_Melikhova">
+                <img src="/src/assets/telegram_icon_131945.svg" alt="Telegram" class="icon">
+              </a>
+            </li>
+            <li>
+              <a href="https://vk.com/melikhova2004">
+                <img src="/src/assets/vk-social-network-logo_icon-icons.com_73339.svg" alt="Vk" class="icon">
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/vasilina04">
+                <img src="/src/assets/github-logo_icon-icons.com_73546.svg" alt="Github" class="icon">
+              </a>
+            </li>
+          </ul></div>
+        </div>
+      <div class = "fact">
+        <h2 class="info">Интересные факты обо мне</h2>
+      <ul>
+        <li>Работаю в творческом коллективе <a href="https://vk.com/tm_kadry">ТМ "Кадры"</a></li>
+        <li>Учусь на втором курсе Прикладной математики и информатики в ДВФУ</li>
+        <li>Главный принцип "Не ошибается тот, кто ничего не делает."</li>
+        <li>Учусь преподавать</li>
+        <li>Люблю сложные задачи с нестандартными решениями</li>
+        <li>Работала в приемной комиссии 2023</li>
+      </ul></div>
+    </body>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
+@media (min-height: 100px) {
   header {
     display: flex;
     place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
   }
 }
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+body {
+    display: inline-block;
+    margin-top: 10%;
+    margin-left: 50%;
+    margin-bottom: 10%;
   }
 
+.My_photo {
+  float: left;
+  border-radius: 10px;
+  border: 5px solid #fff;
+  box-shadow: 2px 1px 5px #999999;
+}
 
-  body.wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.start_info {
+  display: flex;
+  align-items: margin-left;
 }
-  .My_photo {
-    border-radius: 10px;
-    border: 5px solid #fff;
-    box-shadow: 2px 1px 5px #999999;
+
+.profile{
+  display: inline;
+  margin-left: 10%;
 }
-  .icon {
-    width: 48px;
-    height: 48px;
-    margin-left: 12px;
-    margin-right: 12px;
-  }
-  .info{
-    margin-left: 7%;
-    display: flex;
-    place-items: center;
-  }
+
+.icon {
+  width: 48px;
+  height: 48px;
+  margin: 0 12px;
+}
+
+.info {
+  margin-left: 7%;
+  display: flex;
+  align-items: center;
+}
+
+ul.nobullet {
+  display: flex;
+  flex-direction: row;
+  list-style-type: none;
+}
 </style>
