03a6b63711dff3bb363a8d56c01a82099e354285
Vasilina <vasilinamel04@gmail.com>
2023-10-21 12:23:59 +1000

    Изменение шрифта и фона

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,8 @@
         <img src="/src/static/I.jpg" class="My_photo" width="300">
         <div class="profile">
           <h1>Мелихова Василина Андреевна</h1>
-          <h4>Будущий программист - организатор</h4>
-          <h4>Владивосток</h4>
+          <h3>Будущий программист - организатор</h3>
+          <h3>Владивосток</h3>
           <ul class="nobullet">
             <li>
               <a href="https://t.me/Vasilina_Melikhova">
@@ -28,8 +28,8 @@
             </li>
           </ul></div>
         </div>
-        <h2 class ="fact ">Обо мне</h2>
-        <ul class="information">
+        <h2 class ="fact" style="animation: appear 3s 5s ease;">Обо мне</h2>
+        <ul class="information" style="animation: appear 3s 5s ease;">
             <li>Работаю в творческом коллективе <a href="https://vk.com/tm_kadry">ТМ "Кадры"</a></li>
             <li>Учусь на втором курсе Прикладной математики и информатики в ДВФУ</li>
             <li>Главный принцип "Не ошибается тот, кто ничего не делает."</li>
@@ -51,7 +51,7 @@
 .information {
   display: flexbox;
   justify-content: left;
-  margin-left: 12%;
+  margin-left: 17%;
 }
 
 li{
@@ -62,7 +62,7 @@ li{
   justify-content: left;
   /* align-items: center; */
   margin-top: 1%;
-  margin-left: 15%;
+  margin-left: 18%;
   align-items: center;
 }
 
@@ -95,5 +95,14 @@ ul.nobullet {
   display: flex;
   flex-direction: row;
   list-style-type: none;
+}
+
+@keyframes appear {
+    0% {
+        opacity: 0;
+    }
+    100% {
+        opacity: 1;
+    }
 }
 </style>
